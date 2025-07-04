709d1b748676d92f0e9cbdd885015042616065a7
amvalchev <amvr1@icloud.com>
2025-07-04 13:47:24 +0300

    some refactoring and some concepts like interfaces and methods

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,8 +1,22 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 func Perimeter(rectangle Rectangle) float64 {
@@ -11,4 +25,8 @@ func Perimeter(rectangle Rectangle) float64 {
 
 func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
